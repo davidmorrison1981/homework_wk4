60c30d8b82158807e716214e8ccd0f7b03905386
davidmorrison1981 <david.morrison@geminises.com>
2016-08-08 16:32:25 +0100

    Q1

---
+++ day_1/models/details.rb
@@ -0,0 +1,14 @@
+class Information
+
+  def initialize()
+    @name = name
+    @address = address
+    @bio = bio
+  end
+
+  def setup
+    @name = "David Morrison"
+    @address = "Elliot House"
+    @bio = "mechanical engineer lost in the world of ruby"
+  end
+end
